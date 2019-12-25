4424d17bd53334c2bab895be9d9710cd433f514a
JinXin <jinxin_0806@hotmail.com>
2019-12-25 11:41:51 +0800

    数据库

---
+++ 1.sql
@@ -0,0 +1,1 @@
+insert into Companies values('77937cb2-3ae7-4839-b64e-e0d991d15588','alipapa','OK')
---
+++ Controllers/CompanysController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core3.xWebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Core3.xWebApi.Controllers
+{
+    [ApiController]
+    public class CompanysController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+        public CompanysController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        [HttpGet]
+        [Route("Companys/GetCompanies")]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _companyRepository.GetCompany();
+            return new JsonResult(companies);
+        }
+    }
+}
